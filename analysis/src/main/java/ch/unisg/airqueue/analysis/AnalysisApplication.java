75cbfde61d80b7fe00f671223477026d8f6df95f
Marc Soler <m.e.soler@me.com>
2022-03-07 22:28:42 +0100

    Initial version with two modules

---
+++ analysis/src/main/java/ch/unisg/airqueue/analysis/AnalysisApplication.java
@@ -0,0 +1,13 @@
+package ch.unisg.airqueue.analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalysisApplication.class, args);
+	}
+
+}
---
+++ connector/src/main/java/ch/unisg/airqueue/connector/ConnectorApplication.java
@@ -0,0 +1,13 @@
+package ch.unisg.airqueue.connector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectorApplication.class, args);
+	}
+
+}
