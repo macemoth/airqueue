524cd4f8108731f9aafff52e223d1f0e3640a4c7
Marc Soler <m.e.soler@me.com>
2022-04-13 13:48:31 +0200

    Add logger for Booking

--- booking/src/main/java/ch/unisg/airqueue/booking/controller/BookingHttpController.java
+++ booking/src/main/java/ch/unisg/airqueue/booking/controller/BookingHttpController.java
@@ -5,11 +5,15 @@ import ch.unisg.airqueue.booking.domain.Customer;
 import ch.unisg.airqueue.booking.domain.Ticket;
 import ch.unisg.airqueue.booking.messages.Message;
 import ch.unisg.airqueue.booking.messages.MessageSender;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class BookingHttpController {
+
+    private final Logger LOGGER = LogManager.getLogger(this.getClass());
 
     @Autowired
     private MessageSender sender;
@@ -17,13 +21,14 @@ public class BookingHttpController {
     @RequestMapping(path = "/booking", method = RequestMethod.PUT)
     public String bookTicket(@RequestParam("startPort") String startPort,
                              @RequestParam("destinationPort") String destinationPort){
-        System.out.println(startPort + ", " + destinationPort);
+        LOGGER.info("Received Booking from " + startPort + " to " + destinationPort);
         Customer customer = new Customer("Maximilian", "maximilian@familyoffice.uk");
         Ticket ticket = new Ticket(startPort, destinationPort);
         Booking booking = new Booking(customer, ticket);
 
         Message<Booking> message = new Message<>("BookingEvent", booking);
         sender.send(message);
+        LOGGER.info("Sent BookingEvent for " + startPort + " to " + destinationPort);
         return "{\"traceId\": \"" + message.getTraceId() + "\"}";
     }
 }
